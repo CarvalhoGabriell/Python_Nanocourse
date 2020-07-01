77940ebff4242d5a19c13ed4664823cfc4a6459a
CarvalhoGabriell <gabrielfernandes2312@gmial.com>
2020-07-01 18:33:27 -0300

    Finalizando exemplos com dicionarios

--- Capitulo_3_e_4/MangerUsers.py
+++ Capitulo_3_e_4/MangerUsers.py
@@ -1,11 +1,16 @@
 from Pacote_funcoes.identificarFuncoes import *
 
 usuarios = {}
-
-opcao = perguntar()
-
+opcao = menuOpcao()
 while opcao == "I" or opcao == "D" or opcao == "L" or opcao == "P":
     if opcao =="I":
-        inserirUsuario(usuarios)
-opcao = perguntar()
+        inserirUsers(usuarios)
+        print("Usuário inserido com sucesso!!!\n")
+    if opcao == "D":
+        deletarUsers(usuarios, input("Qual usuário deseja deletar? ").upper())
+    if opcao == "L":
+        listarUser(usuarios)
+    if opcao == "P":
+        pesquisaUsers(usuarios, input("Qual login deseja pesquisar? ").upper())
+    opcao = menuOpcao()
 
