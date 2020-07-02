6cd2090b9bccdae45cd8ab11d050449da76d06d7
CarvalhoGabriell <gabrielfernandes2312@gmial.com>
2020-07-02 15:57:13 -0300

    Inciando novo script

--- Manipilando_Arquivos/Inventario.py
+++ Manipilando_Arquivos/Inventario.py
@@ -11,12 +11,8 @@ while 0 < opcao < 4:
     elif opcao == 3:
         resultado = lendoExibindo()
         for linha in resultado:
-            separacao = linha[linha.find(";")+1:-1]
-            data = separacao[0:separacao.find(";")]
-            separacao = separacao[separacao.find(";")+1:-1]
-            descricao = separacao[0:separacao.find(";")]
-            departamento = linha[linha.rfind(";")+1:-1]
-            print("DATA..........:", data)
-            print("DESCRIÇÃO.....:", descricao)
-            print("DEPARTAMENTO..:", departamento)
+            lista = linha.split(";")
+            print("DATA..........:", lista[1])
+            print("DESCRIÇÃO.....:", lista[2])
+            print("DEPARTAMENTO..:", lista[3])
     opcao = mostrarMenu()
