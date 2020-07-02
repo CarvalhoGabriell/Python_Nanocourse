eed78888b26886fcd126850f030bf05ea1ae7bfe
CarvalhoGabriell <gabrielfernandes2312@gmial.com>
2020-07-02 11:29:20 -0300

    Tuplas avançadas, finalizado.

---
+++ Capitulo_3_e_4/TuplasEnumerat.py
@@ -0,0 +1,19 @@
+usuarios = {}
+resp = "SIM"
+emails = []
+
+while resp == "SIM":
+    emails.append(input("Informe seu email: ").lower())
+    resp = input("Digite <SIM> para continuar inserindo <EMAILS>\n").upper()
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+tupla = list(enumerate(emails))
+
+for chave in range(0, len(tupla)):
+    print("Email...: ", tupla[chave][1], ",", tupla[chave][0])
+    usuarios[tupla[chave]]= [input("Digite seu nome: "), input("Digite seu nível de acesso: ").upper()]
+
+for chave, dado in usuarios.items():
+    print("Usuario..: ", chave[0])
+    print("Email....: ", chave[1])
+    print("Nome.....: ", dado[0])
+    print("Nível....: ", dado[1])
