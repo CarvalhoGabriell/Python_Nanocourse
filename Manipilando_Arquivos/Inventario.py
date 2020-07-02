8ce75859430e4cc0262dd06a97f0287627305af7
CarvalhoGabriell <gabrielfernandes2312@gmial.com>
2020-07-02 15:45:50 -0300

    Craindo script para manipular arquivos

--- Manipilando_Arquivos/Inventario.py
+++ Manipilando_Arquivos/Inventario.py
@@ -5,11 +5,18 @@ inventario = {}
 opcao = mostrarMenu()
 while 0 < opcao < 4:
     if opcao == 1:
-        resp = "SIM"
-        while resp == "SIM":
-            registarAtivo(inventario)
+        registarAtivo(inventario)
     elif opcao == 2:
         persistir(inventario)
     elif opcao == 3:
-        lendoExibindo()
+        resultado = lendoExibindo()
+        for linha in resultado:
+            separacao = linha[linha.find(";")+1:-1]
+            data = separacao[0:separacao.find(";")]
+            separacao = separacao[separacao.find(";")+1:-1]
+            descricao = separacao[0:separacao.find(";")]
+            departamento = linha[linha.rfind(";")+1:-1]
+            print("DATA..........:", data)
+            print("DESCRIÇÃO.....:", descricao)
+            print("DEPARTAMENTO..:", departamento)
     opcao = mostrarMenu()
