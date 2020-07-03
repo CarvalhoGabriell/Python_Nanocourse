29ca1007b785c3c733489c8407449fe07f8dad23
CarvalhoGabriell <gabrielfernandes2312@gmial.com>
2020-07-03 12:51:12 -0300

    Exercicio para manipular uma string

---
+++ Cap1_E_2_Variaveis_E_Loopings/EncontrarStrings.py
@@ -0,0 +1,9 @@
+palavra = "Estudante FIAP"
+
+print(palavra[0:9])
+print(palavra[10:])
+
+print(palavra.find("a"))  # ENCONTRA A PRIMEIRA OCORRENCIA DO CARACTER
+
+palavra_2 = "Eu amo Python"
+print(palavra_2.split(" "))  # RETORNA UMA LISTA COM OS VALORES
