a41bb2c3f9a2990b325287ccbf1584df93976411
CarvalhoGabriell <gabrielfernandes2312@gmial.com>
2020-07-05 17:02:06 -0300

    Finalizando chat entre servidor e cliente.

--- Library_Socket/Cliente.py
+++ Library_Socket/Cliente.py
@@ -2,12 +2,13 @@ from socket import *
 
 servidor="127.0.0.1"
 porta=43210
-
-msg =bytes(input("Digite algo aqui: "), 'utf-8')  # <BYTES> para converte em bytes é necessario dois parametros (conteudo, padrão)
-obj_socket = socket(AF_INET, SOCK_STREAM)
-obj_socket.connect((servidor, porta))
-obj_socket.send(msg)
-
-resposta =obj_socket.recv(1024)
-print("Recebemos: ", resposta)
+while True:
+    obj_socket= socket(AF_INET, SOCK_STREAM)
+    obj_socket.connect((servidor, porta))
+    msg= bytes(input("Digite sua mensagem aqui: "), 'utf-8')  # <BYTES> para converte em bytes é necessario dois parametros (conteudo, padrão)
+    obj_socket.send(msg)
+    resposta= obj_socket.recv(1024)
+    print("Resposta do Servidor...: ", str(resposta)[2:-1])
+    if str(msg)[2:-1].upper() == "FIM":
+        break
 obj_socket.close()
