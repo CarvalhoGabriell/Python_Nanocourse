4aaeffc7c13e46a807f6ad61414d80a73d132aca
CarvalhoGabriell <gabrielfernandes2312@gmial.com>
2020-07-05 16:40:53 -0300

    Fazendo testes e pequenas alterações

--- Cap3_E_4_Tuplas_E_Dicionarios/Tuplas.py
+++ Cap3_E_4_Tuplas_E_Dicionarios/Tuplas.py
@@ -3,11 +3,9 @@ emails = ["gagabiru@io.com", "zikaBala@hot.com"]
 
 tupla = list(enumerate(emails))
 # print(tupla)
-
 for chave in range(0, len(tupla)):
     print("Email: ", tupla[chave][1])
     usuarios[tupla[chave]] = [input("Informe seu nome: "), input("Digite seu nível: ").upper()]
-
 
 for chave, dado in usuarios.items():  # O metodo ITENS , sempre retorna uma TUPLA
     print("Usuário:............ ", chave[0])
--- Cap3_E_4_Tuplas_E_Dicionarios/dicionarioExemplo.py
+++ Cap3_E_4_Tuplas_E_Dicionarios/dicionarioExemplo.py
@@ -4,15 +4,14 @@ usuarios = {
     "gabriel": ["23/12/1999", "lab_livre", "Gabriel Carvalho"],
     "sindy": ["14/02/2019", "wow_lab", "Syndyca Araujo"],
     "felipe": ["18/05/2018", "acesso_remoto", "Felipe Carvalho"],
-    "felipe": ["03/07/2011", "primeiro_acesso", "Felipe Carvalho Fernandes"]
+    "felipe": ["03/07/2011", "primeiro_acesso", "Felipe Carvalho Fernandes"],
 }
 print(usuarios, "\n")
 
 # Criando dicionários de forma singular, assim fazendo de forma em OBJETO a OBJETO
 usuarios["cleybson"] = ["27/06/2018", "lab_01", "Cleybson Roger"]
-usuarios["Jonas"] = ["04/08/2019", "primeiro_acesso", "Jonas Khanvald"]
+usuarios["gabriel"] = ["04/08/2019", "primeiro_acesso", "Jonas Khanvald"]
 print(usuarios)
 
 # Metodo GET serve poara retornar os valores passados entre os "".
 print(usuarios.get("gabriel"))
-
